55c8df6bfd26a2665c059d9416379532507beda5
Eduardo Marturano <emartura@boldt.com.ar>
2021-02-26 00:27:35 -0300

    2021 02 25

--- src/main/java/ar/com/coninf/doconline/rest/model/tx/DatoQr.java
+++ src/main/java/ar/com/coninf/doconline/rest/model/tx/DatoQr.java
@@ -17,9 +17,9 @@ public class DatoQr implements Serializable {
 	private Long    importe; 		//Decimal hasta 13 enteros y 2 decimales		OBLiGATORiO 		importe Total del comprobante (en la moneda en la que fue emitido)						12100			
 	private String  moneda; 		//3 caracteres									OBLiGATORiO 		Moneda del comprobante (segun Tablas del sistema )										"DOL"			
 	private Long    ctz; 			//Decimal hasta 13 enteros y 6 decimales		OBLiGATORiO 		Cotizacion en pesos argentinos de la moneda utilizada (1 cuando la moneda sea pesos)	65				
-	private Integer tipoDocRc; 		//Numerico hasta 2 digitos						DE CORRESPONDER 	Codigo del Tipo de documento del receptor (segun Tablas del sistema )					80				
-	private Long    nroDocRe; 		//Numerico hasta 20 digitos						DE CORRESPONDER 	Numero de documento del receptor correspondiente al tipo de documento indicado			20000000001		
-	private String  tipoCodAt; 		//string										OBLiGATORiO 		"A" para comprobante autorizado por CAEA, "E" para comprobante autorizado por CAE		"E"				
+	private Integer tipoDocRec; 	//Numerico hasta 2 digitos						DE CORRESPONDER 	Codigo del Tipo de documento del receptor (segun Tablas del sistema )					80				
+	private Long    nroDocRec; 		//Numerico hasta 20 digitos						DE CORRESPONDER 	Numero de documento del receptor correspondiente al tipo de documento indicado			20000000001		
+	private String  tipoCodAut; 	//string										OBLiGATORiO 		"A" para comprobante autorizado por CAEA, "E" para comprobante autorizado por CAE		"E"				
 	private String  codAut; 		//Numerico 14 digitos							OBLiGATORiO 		Codigo de autorizacion otorgado por AFiP para el comprobante							70417054367476	
 	
 }
